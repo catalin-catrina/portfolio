255aa66d0a51a5b3f6d5adeaf422d5d540c01aca
Catalin Catrina <catalin.catrina@outlook.com>
2022-06-17 23:29:44 +0300

    bulma fixed cached .git file

---
+++ kanban/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MainViewComponent } from './pages/main-view/main-view.component';
+
+@NgModule({
+  declarations: [AppComponent, MainViewComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
