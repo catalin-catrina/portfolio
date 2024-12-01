e4ed22be9bd167c9470120dea570e9e90a1298e1
Catalin Catrina <catalin.catrina@outlook.com>
2024-12-01 22:18:36 +0200

    [Social Media Angular App] Started working on likes functionality

--- social-media-angular-app/src/app/services/likes.service.ts
+++ social-media-angular-app/src/app/services/likes.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LikesService {
+  constructor() {}
 
-  constructor() { }
+  likePost(postId: string, userId: string) {}
+
+  unlikePost(postId: string, userId: string) {}
+
+  hasUserLikedPost(postId: string, userId: string): Promise<boolean> {}
+
+  getLikesCount(postId: string): Promise<number> {}
 }
